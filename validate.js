1957634dfd70755397cd609816cb78188d93c4dd
Pasuta-data <pasuta.p2001@gmail.com>
2025-04-18 15:59:13 +0700

    Bug fix#3 – revise candidate no. validation

--- validate.js
+++ validate.js
@@ -1,32 +1,33 @@
 function checkSID() {
-  let sid = (document.getElementById("sid").value).trim();
-  if (sid.length == 10) {
-    return true;
-  } else {
-    return false;
-  }
+	let sid = (document.getElementById("sid").value).trim();
+	if (sid.length === 10 && !isNaN(sid)) {
+		return true;
+	} else {
+		return false;
+	}
 }
+
 
 function checkCandiNo() {
-  let candi = (document.getElementById("candi").value).trim();
-  if (isNaN(candi) || candi < 1) {
-    return false;
-  } else {
-    return true;
-  }
+	let candi = (document.getElementById("candi").value).trim();
+	if (isNaN(candi) || candi.length < 1 || candi.length > 10) {
+		return false;
+	} else {
+		return true;
+	}
 }
 
-function validateForm(){
-	if(!checkSID()){
-	  alert("Invalid value for Student ID!");
-	  document.getElementById("sid").focus();
-	  return false;
-	}else{
-		if(!checkCandiNo()){
-		  alert("Invalid value for Candidate No!!");
-		  document.getElementById("candi").focus();
-		  return false;
-		}else{
+function validateForm() {
+	if (!checkSID()) {
+		alert("Invalid value for Student ID!");
+		document.getElementById("sid").focus();
+		return false;
+	} else {
+		if (!checkCandiNo()) {
+			alert("Invalid value for Candidate No!!");
+			document.getElementById("candi").focus();
+			return false;
+		} else {
 			alert("Your input data passes validation!!");
 			return true;
 		}
